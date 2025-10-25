0824c430bc1a128978075c489bf4c775ad9babae
Cleiton dos Santos <cleiton.jnet@gmail.com>
2025-10-25 15:51:54 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,9 @@
 package application;
 
-import boardgame.Position;
-
-import static java.lang.IO.println;
+import boardgame.Board;
 
 public class Program {
     void main() {
-        Position pos = new Position(3,5);
-        println(pos);
+        Board board = new Board(8,8);
     }
 }
