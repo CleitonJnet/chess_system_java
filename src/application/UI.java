caef0c9f99a333a4d0af10ca909a2b619e722dfc
Cleiton dos Santos <cleiton.jnet@gmail.com>
2025-10-25 18:31:49 -0300

    Litle improvement in board printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,11 +1,31 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 import static java.lang.IO.print;
 import static java.lang.IO.println;
 
 public class UI {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
             print((8 - i) + " ");
@@ -21,7 +41,11 @@ public class UI {
         if (piece == null) {
             print("-");
         }else {
-            print(piece);
+            if (piece.getColor() == Color.WHITE) {
+                print(ANSI_WHITE + piece + ANSI_RESET);
+            } else {
+                print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
         }
         print(" ");
     }
